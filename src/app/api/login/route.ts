4a168594a309a49a6fbdf34d6805a5004870af2b
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-07-24 10:10:45 -0300

    feat: criando rotas login e register api nextjs

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,69 @@
+import prisma from '@/app/lib/db'
+import { NextResponse } from 'next/server'
+import { createHash } from 'node:crypto'
+import { userCreateToken } from '../register/route'
+const jwt = require('jsonwebtoken')
+
+const compareHash = (password: string, originalHash: string) => {
+  const newHash = createHash('md5').update(password).digest('hex')
+
+  return newHash === originalHash
+}
+
+const newToken = (data: userCreateToken) => {
+  const token = jwt.sign({ data }, 'teste-ronan', {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  })
+  return token
+}
+
+export async function POST(req: Request) {
+  const { email, password } = await req.json()
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: 'Login e/ou senha incorretos.',
+        },
+        { status: 404 },
+      )
+    }
+
+    const hashOriginal = user.password
+    const decrypt = compareHash(password, hashOriginal)
+    if (decrypt !== true) {
+      console.log('esta aqui')
+      return NextResponse.json(
+        {
+          message: 'Login e/ou senha incorretos.',
+        },
+        { status: 404 },
+      )
+    }
+
+    const { password: _, ...userWithoutPassword } = user
+    const token = newToken(userWithoutPassword)
+
+    return NextResponse.json(
+      { ...userWithoutPassword, token },
+
+      { status: 200 },
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'error',
+        error,
+      },
+      { status: 500 },
+    )
+  }
+}
---
+++ src/app/api/register/route.ts
@@ -0,0 +1,72 @@
+import prisma from '@/app/lib/db'
+import { NextResponse } from 'next/server'
+import { createHash } from 'node:crypto'
+const jwt = require('jsonwebtoken')
+
+export interface UserCreate {
+  name: string
+  email: string
+  password: string
+}
+
+export interface UserReturn extends UserCreate {
+  id: number
+}
+
+export interface userCreateToken {
+  name: string
+  email: string
+}
+
+export interface UserWithToken {
+  id: number
+  name: string
+  email: string
+  token: string
+}
+
+const createPassword = (password: string) => {
+  const newHash = createHash('md5').update(password).digest('hex')
+
+  return newHash
+}
+
+const newToken = (data: userCreateToken) => {
+  const token = jwt.sign({ data }, 'teste-ronan', {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  })
+  return token
+}
+
+export async function POST(req: Request) {
+  const { name, email, password } = await req.json()
+
+  try {
+    const decrypt = createPassword(password)
+    const register: UserReturn = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: decrypt,
+      },
+    })
+
+    const { password: _, ...userWithoutPassword } = register
+    const token = newToken(userWithoutPassword)
+
+    return NextResponse.json(
+      { ...userWithoutPassword, token },
+
+      { status: 200 },
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'error',
+        error,
+      },
+      { status: 500 },
+    )
+  }
+}
